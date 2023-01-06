10c4366cfef60d9042d219d7bfe50574119b9abf
xoxoiindy <proanocindy@gmail.com>
2023-01-06 02:49:45 -0700

    Add project files.

---
+++ CRUD Web Application/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using CRUD_Web_Application.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CRUD_Web_Application.Data
+{
+    public class ApplicationDbContext :DbContext  
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) :base(options) 
+        {
+
+        }
+
+        //references the models folder ->category.cs - getter & setters
+        //Databse set for the Category Table 
+        public  DbSet<Category> Categories { get; set; }
+    }
+}
---
+++ CRUD Web Application/Models/Category.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CRUD_Web_Application.Models
+{
+    public class Category
+    {
+        //Id is the primary key & identity column - Key is attribute 
+        [Key]
+        public int Id { get; set; }
+
+        //Name = required for validation
+        [Required]
+        public string Name { get; set; }
+
+        public int DisplayOrder { get; set; }
+
+        public DateTime CreateDateTime { get; set; } = DateTime.Now;
+
+
+
+    }
+}
